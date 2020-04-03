openvinotoolkit/training_extensions

Fix conversion script

pytorch_toolkit/person_reidentification/convert_to_onnx.py
@@ -51,8 +51,7 @@ def main():
             pretrained=False,
             use_gpu=True,
             feature_dim=cfg.model.feature_dim,
-            fpn=cfg.model.fpn,
-            fpn_dim=cfg.model.fpn_dim,
+            fpn_cfg=cfg.model.fpn,
             pooling_type=cfg.model.pooling_type,
             input_size=(cfg.data.height, cfg.data.width),
             dropout_cfg=cfg.model.dropout,

sovrasov/openvino_training_extensions

pytorch_toolkit/person_reidentification/convert_to_onnx.py
@@ -56,7 +56,8 @@ def main():
             input_size=(cfg.data.height, cfg.data.width),
             dropout_cfg=cfg.model.dropout,
             IN_first=cfg.model.IN_first,
-            extra_blocks=cfg.model.extra_blocks
+            extra_blocks=cfg.model.extra_blocks,
+            lct_gate=cfg.model.lct_gate
         )
 
     load_pretrained_weights(model, cfg.model.load_weights)